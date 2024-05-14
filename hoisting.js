e78b27055cd117ee739455369fbefdc1af9e026a
gerba <novokmet.ivan@gmail.com>
2024-05-14 17:18:09 +0200

    metode js, varijable

---
+++ hoisting.js
@@ -0,0 +1,17 @@
+var x = 2;
+var y = 3;
+
+var z = x + y;
+console.log(z);
+
+function zbroji() {
+
+    console.log("1. n=" + n);
+    var n = 3;
+    console.log("2. n=" + n);
+    let m = 4;
+    console.log(n + m)
+}
+
+zbroji();
+
